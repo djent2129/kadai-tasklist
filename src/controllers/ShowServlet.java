116846dbc7057cafa994c21ef095c203f23f75b2
Motoki.Nakamura <djent2129@outlook.jp>
2021-03-27 18:03:49 +0900

    Add IndexServlet.java

--- src/controllers/ShowServlet.java
+++ src/controllers/ShowServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Message;
+import models.Task;
 import utils.DBUtil;
 /**
  * Servlet implementation class ShowServlet
@@ -34,12 +34,12 @@ public class ShowServlet extends HttpServlet {
     EntityManager em = DBUtil.createEntityManager();
 
     //該当のIDのメッセージ1件のみデータベースから取得
-    Message m = em.find(Message.class, Integer.parseInt(request.getParameter("id")));
+    Task m = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
 
     em. close();
 
     //メッセージをリクエストスコープにセットしてshow.jspを呼び出す
-    request.setAttribute("message", m);
+    request.setAttribute("task", m);
 
     RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/show.jsp");
     rd.forward(request, response);
--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Message;
+import models.Task;
 import utils.DBUtil;
 
 /**
@@ -38,7 +38,7 @@ public class UpdateServlet extends HttpServlet {
 
             //セッションスコープからメッセージのIDを取得して
             //該当のIDのメッセージ１件のみをデータベースから取得
-            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
+            Task m = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
 
             //フォームの内容を各フィールドの上書き
             String content = request.getParameter("content");
@@ -52,7 +52,7 @@ public class UpdateServlet extends HttpServlet {
             em.close();
 
             //セッションスコープ上の不要となったデータを削除
-            request.getSession().removeAttribute("message_id");
+            request.getSession().removeAttribute("task_id");
 
             //indexページへリダイレクト
             response.sendRedirect(request.getContextPath() + "/index");
